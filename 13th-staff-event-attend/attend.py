bb04303ea0bc493a47f7a31cb98efb4767d552e9
Felwyn <tjhsutherland@gmail.com>
2023-05-19 00:25:38 +0100

    upload test

---
+++ 13th-staff-event-attend/attend.py
@@ -0,0 +1,13 @@
+from redbot.core import commands
+
+class 13th_attendtest(commands.Cog):
+    """test"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx):
+        """simple call and response"""
+        # Your code will go here
+        await ctx.send("pong")
