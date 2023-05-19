c976a13a72b3e0343f7447d38ea459e88d06ae0a
Felwyn <tjhsutherland@gmail.com>
2023-05-19 04:02:01 +0100

    addition of interactions

--- attend/attend.py
+++ attend/attend.py
@@ -1,14 +1,31 @@
 import discord
 from redbot.core import commands, app_commands
+from enum import Enum
+
+class roles(Enum):
+    Owner = "owner"
+    Dancer = "dancer"
+
 
 class attendtest(commands.Cog):
     """test"""
-
     def __init__(self, bot):
         self.bot = bot
 
+#command 1
     @app_commands.command()
     async def test(self, interaction: discord.Interaction):
         """simple call and response"""
         # Your code will go here
         await interaction.response.send_message("test", ephemeral=True)
+
+
+#command 2
+    @app_commands.command(name="awa")
+    @app_commands.describe(roles="role to sign up as")
+    async def testint(self, interaction: discord.Interaction, test1: Roles):
+        """less simple, interaction decision"""
+        # Your code will go here
+        await interaction.response.send_message("role is {test1.value}", ephemeral=True)
+        
+        
